6a6f78d9b2479184b8249622ad2293a0a8bf359f
GideonSenku <gentlewind2391@gmail.com>
2021-09-26 16:44:00 +0800

    feat: version 0.1.1

--- figma-image-upload.user.js
+++ figma-image-upload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Figma Image Upload
 // @namespace   https://github.com/gideonsenku
-// @version     0.1.0
+// @version     0.1.1
 // @description Figma Image Upload图片上传工具
 // @encoding    utf-8
 // @author      gideonsenku
@@ -10,7 +10,7 @@
 // @updateURL   https://github.com/gideonsenku/figma-image-upload/raw/master/figma-image-upload.user.js
 // @downloadURL https://github.com/gideonsenku/figma-image-upload/raw/master/figma-image-upload.user.js
 // @match       *://www.figma.com/file/*
-// @match       https://nocoding.xyz/figma-image-upload/setting
+// @match       https://nocoding.xyz/figma-image-upload/setting.html
 // @run-at      document-start
 // @icon        https://www.google.com/s2/favicons?domain=figma.com
 // @license     MIT; https://github.com/gideonsenku/figma-image-upload/blob/main/LICENSE
--- rollup.config.js
+++ rollup.config.js
@@ -42,7 +42,7 @@ module.exports = {
       const items = [
         ['name', 'Figma Image Upload'],
         ['namespace', 'https://github.com/gideonsenku'],
-        ['version', '0.1.0'],
+        ['version', '0.1.1'],
         ['description', 'Figma Image Upload图片上传工具'],
         ['encoding', 'utf-8'],
         ['author', 'gideonsenku'],
